289c41127cb27498c9a9ab8980ca28326d9881f4
João Vitor <61944386+Drack112@users.noreply.github.com>
2025-01-09 18:49:10 -0300

    feat: AppProvider for the Auth client and React-Query

---
+++ apps/web/src/providers/app-provider.tsx
@@ -0,0 +1,26 @@
+import {
+  HydrationBoundary as Hydrate,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+import type { AppProps } from 'next/app'
+import { SessionProvider } from 'next-auth/react'
+
+import { queryClient } from '@/api'
+
+export const AppProvider = (props: {
+  children: React.ReactNode
+  pageProps: AppProps['pageProps']
+}) => {
+  const {
+    children,
+    pageProps: { session, ...pageProps },
+  } = props
+
+  return (
+    <SessionProvider session={session}>
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>{children}</Hydrate>
+      </QueryClientProvider>
+    </SessionProvider>
+  )
+}
