c23dc54f0ee996c18426c14dd34ef34d65bc5d4b
João Vitor <61944386+Drack112@users.noreply.github.com>
2025-01-09 18:56:56 -0300

    feat: user entity and account entity resolver

---
+++ apps/web/src/server/schema/user/user-resolver.ts
@@ -0,0 +1,59 @@
+import { LibsqlError } from '@libsql/client'
+import argon2 from 'argon2'
+import { eq, getTableColumns } from 'drizzle-orm'
+import {
+  Arg,
+  Ctx,
+  FieldResolver,
+  Mutation,
+  Query,
+  Resolver,
+  Root,
+} from 'type-graphql'
+
+import { dbErrorCodes } from '@/constants'
+import { db } from '@/db/db'
+import { Accounts, Users } from '@/db/schema'
+import type { Context } from '@/types'
+
+import { UpdateUserOutput, User, UserInput } from './user'
+
+@Resolver(User)
+export class UserResolver {
+  @Query(() => User)
+  async me(@Ctx() ctx: Context): Promise<User> {
+    const session = ctx.session
+
+    if (!session?.user) {
+      throw new Error('Unauthorized')
+    }
+
+    const userWithAccounts = await db
+      .select()
+      .from(Users)
+      .leftJoin(Accounts, eq(Accounts.userId, Users.id))
+      .where(eq(Users.id, session.user.id))
+
+    const user = userWithAccounts[0]
+
+    if (!user.users) {
+      throw new Error('User not found')
+    }
+
+    const accounts = userWithAccounts
+      .filter((userWithAccount) => !!userWithAccount.accounts)
+      .map((userWithAccount) => userWithAccount.accounts)
+
+    return {
+      id: user.users.id,
+      name: user.users.name,
+      username: user.users.username,
+      email: user.users.email ?? undefined,
+      createdAt: user.users.createdAt,
+      updatedAt: user.users.updatedAt,
+      accounts: accounts.map((account) => ({
+        provider: account!.provider,
+      })),
+    }
+  }
+}
