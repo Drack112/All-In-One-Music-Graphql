7af8d55d72a46e4e784b5b8676230d639907c32c
João Vitor <61944386+Drack112@users.noreply.github.com>
2025-01-09 18:56:20 -0300

    feat: constants for errors treatment

---
+++ apps/web/src/constants.ts
@@ -0,0 +1,20 @@
+import type { ArtistSortableProperties } from '@/store/use-local-settings'
+
+export const playlistType = {
+  PLAYLIST: 1,
+  RADIO: 2,
+} as const
+
+export const dbErrorCodes = {
+  SQLITE_CONSTRAINT_UNIQUE: 'SQLITE_CONSTRAINT_UNIQUE',
+} as const
+
+export const queryKeys = {
+  videoInfo: (query: string) => ['getVideoInfo', query],
+}
+
+export const sortablePropertiesMapping = {
+  default: 'default',
+  title: 'title',
+  scrobbles: 'playcount',
+} as const satisfies Record<ArtistSortableProperties, string>
