e128bee4019bb35f6787282c63afb2a9aab4a342
João Vitor <61944386+Drack112@users.noreply.github.com>
2025-01-09 21:24:06 -0300

    fix: api provider with queries

--- apps/web/src/api.ts
+++ apps/web/src/api.ts
@@ -1,8 +1,11 @@
 import { QueryClient } from '@tanstack/react-query'
 import { GraphQLClient } from 'graphql-request'
 import { getBaseURL } from './utils/get-base-url'
+import { getSdk } from 'music-shared'
 
 const gqlClient = new GraphQLClient(`${getBaseURL()}/api/graphql`)
+
+export const { meQuery } = getSdk(gqlClient)
 
 export const queryClient = new QueryClient({
   defaultOptions: {
