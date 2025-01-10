7f8a1fb7cbccecaf1636e50fc93579a475da2a25
João Vitor <61944386+Drack112@users.noreply.github.com>
2025-01-09 22:39:57 -0300

    feat: modal provider

---
+++ apps/web/src/components/modal/modal.tsx
@@ -0,0 +1,68 @@
+import { useModalStore } from '@/store/use-modal'
+import {
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+  Transition,
+  TransitionChild,
+} from '@headlessui/react'
+import { XMarkIcon } from '@heroicons/react/24/outline'
+import { Fragment } from 'react'
+import { Button } from '../button'
+
+export function Modal() {
+  const isOpen = useModalStore((state) => state.isOpen)
+  const content = useModalStore((state) => state.content)
+  const closeModal = useModalStore((state) => state.closeModal)
+
+  return (
+    <>
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as='div' className='relative z-50' onClose={closeModal}>
+          <TransitionChild
+            as={Fragment}
+            enter='ease-out duration-300'
+            enterFrom='opacity-0'
+            enterTo='opacity-100'
+            leave='ease-in duration-200'
+            leaveFrom='opacity-100'
+            leaveTo='opacity-0'
+          >
+            <div className='fixed inset-0 bg-black/40' />
+          </TransitionChild>
+          <div className='fixed inset-0'>
+            <div className='flex min-h-full items-center justify-center p-4 text-center '>
+              <TransitionChild
+                as={Fragment}
+                enter='ease-out duration-300'
+                enterFrom='opacity-0 scale-95'
+                enterTo='opacity-100 scale-100'
+                leave='ease-in duration-200'
+                leaveFrom='opacity-100 scale-100'
+                leaveTo='opacity-0 scale-95'
+              >
+                <DialogPanel className='max-h-[80svh] w-fit overflow-y-auto overflow-x-clip rounded-2xl border border-solid border-zinc-900 bg-surface-900 text-left align-middle shadow-xl transition-all'>
+                  <DialogTitle
+                    as='h3'
+                    className='sticky top-0 flex items-center justify-between rounded-t-2xl bg-surface-700/30 p-4 text-lg font-medium leading-6'
+                  >
+                    <span>{content.title}</span>
+                    <Button
+                      type='button'
+                      variant='ghost'
+                      className='inline-flex text-gray-300'
+                      onClick={closeModal}
+                    >
+                      <XMarkIcon className='size-6' />
+                    </Button>
+                  </DialogTitle>
+                  {content.content}
+                </DialogPanel>
+              </TransitionChild>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  )
+}
