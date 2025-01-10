0ebff2c4a287f2aed16724d6979ddf627ddf4957
João Vitor <61944386+Drack112@users.noreply.github.com>
2025-01-09 22:40:16 -0300

    feat: layout with the modal provider and Attribution component

--- apps/web/src/layout/main.tsx
+++ apps/web/src/layout/main.tsx
@@ -1,4 +1,32 @@
 import { Menu } from '@/components/menu'
+import { Modal } from '@/components/modal/modal'
+import { Toaster } from 'sonner'
+import { Icon } from '@iconify/react'
+import { useLayoutState } from '@/store/use-layout-state'
+
+const Attribution = () => {
+  const theaterMode = useLayoutState((state) => state.theaterMode)
+
+  if (theaterMode) return null
+
+  const currentYear = new Date().getFullYear()
+
+  return (
+    <div className='mx-auto flex w-fit items-center gap-1 py-4'>
+      <p className='text-xs text-gray-400'>
+        Made with ❤️ for music | {currentYear} Drack |
+      </p>
+      <a
+        href='https://github.com/Drack112/All-In-One-Music-Graphql'
+        target='_blank'
+        rel='noreferrer noopener'
+        className='transition-colors hover:text-primary-500'
+      >
+        <Icon icon='mdi:github' className='size-4 shrink-0' />
+      </a>
+    </div>
+  )
+}
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -8,6 +36,10 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
           <Menu />
           {children}
         </main>
+        <Attribution />
+        <div className='h-28' />
+        <Toaster />
+        <Modal />
       </div>
     </>
   )
