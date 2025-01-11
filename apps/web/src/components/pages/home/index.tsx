e255931c9565bc7939db3feea60c436cb6b752cd
João Vitor <61944386+Drack112@users.noreply.github.com>
2025-01-11 18:36:29 -0300

    feat: Basic home page setup

--- apps/web/src/components/pages/home/index.tsx
+++ apps/web/src/components/pages/home/index.tsx
@@ -1,3 +1,52 @@
+import { AnimatedBeam } from '@/components/beam/animated'
+import { Gradient } from '@/components/gradient'
+import { SearchTrigger } from '@/components/search/trigger'
+
+import { Seo } from '@/components/seo'
+import { useGlobalSearchStore } from '@/store/use-global-search'
+import { useLayoutState } from '@/store/use-layout-state'
+
 export const HomePage = () => {
-  return <div>Hello</div>
+  const { setIsOpen } = useGlobalSearchStore()
+
+  const theaterMode = useLayoutState((state) => state.theaterMode)
+
+  const renderContent = () => {
+    if (theaterMode) {
+      return <div>Mode</div>
+    }
+
+    return (
+      <>
+        <div className='container relative mx-auto flex h-full flex-col items-center px-6 md:flex-row md:px-8 lg:gap-4 '>
+          <Gradient />
+          <div className='flex grow flex-col'>
+            <h1
+              className='mb-4 mt-8 text-balance text-3xl text-gray-100 lg:-mt-4 lg:text-4xl'
+              id='home'
+            >
+              Listen to your favorite artists <br /> and create your own
+              playlists!
+            </h1>
+            <h2 className='text-lg text-zinc-400'>
+              Enjoy unlimited free music, full albums, lyrics, import playlists{' '}
+              and much more!
+            </h2>
+            <SearchTrigger
+              className='z-10 mt-6 h-12 px-5 py-7'
+              onClick={() => setIsOpen(true)}
+            />
+          </div>
+          <AnimatedBeam className='mx-auto mt-10 h-2/5 md:pl-8 lg:mt-0' />
+        </div>
+      </>
+    )
+  }
+
+  return (
+    <>
+      <Seo description='Enjoy unlimited free music, find similar artists, full albums, lyrics and much more!' />
+      {renderContent()}
+    </>
+  )
 }
