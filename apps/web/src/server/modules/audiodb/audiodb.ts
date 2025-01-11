ca391cbb2f8610892b29ec34b74f27d311ad8749
João Vitor <61944386+Drack112@users.noreply.github.com>
2025-01-11 12:51:37 -0300

    feat: audioDB module

---
+++ apps/web/src/server/modules/audiodb/audiodb.ts
@@ -0,0 +1,40 @@
+import { createQueryParam } from '@/utils/create-query-param'
+import Axios from 'axios'
+import { oneLineTrim } from 'common-tags'
+import { AlbumTracks, SearchAlbums, SearchArtist } from './types'
+
+type AudioDBMethods = 'searchalbum' | 'search' | 'track'
+
+interface AudioDBParams {
+  track?: string
+  artist?: string
+  album?: string
+  albumId?: string
+}
+
+const getEndpoint = (method: string) =>
+  `https://www.theaudiodb.com/api/v1/json/${process.env.AUDIODB_API_KEY}/${method}.php?`
+
+const audioDB = async <T>(method: AudioDBMethods, args: AudioDBParams) => {
+  const { album, albumId, artist, track } = args
+
+  const url = oneLineTrim`${getEndpoint(method)}
+    ${createQueryParam({ s: track || artist, a: album, m: albumId })}`
+
+  return Axios.get<T>(url, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+}
+
+audioDB.getAlbumsByArtist = (args: AudioDBParams) =>
+  audioDB<SearchAlbums>('searchalbum', args)
+
+audioDB.getArtist = (args: AudioDBParams) =>
+  audioDB<SearchArtist>('search', args)
+
+audioDB.getAlbumTracks = (args: AudioDBParams) =>
+  audioDB<AlbumTracks>('track', args)
+
+export { audioDB }
