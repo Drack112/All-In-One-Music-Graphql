4df4130d6b1cf086123e505afacba18bd3e778a1
João Vitor <61944386+Drack112@users.noreply.github.com>
2025-01-11 11:25:32 -0300

    feat: component search trigger

---
+++ apps/web/src/components/search/trigger/index.tsx
@@ -0,0 +1,23 @@
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline'
+
+interface TriggerProps {
+  className?: string
+  onClick?: () => void
+}
+
+export const SearchTrigger = (props: TriggerProps) => {
+  return (
+    <div className='pointer-events-auto relative contents rounded-xl'>
+      <button
+        type='button'
+        onClick={props.onClick}
+        className={`flex items-center rounded-xl bg-surface-950 leading-6 text-gray-300/80 shadow-sm ring-2 ring-surface-800 transition-all hover:ring-surface-700 hover:brightness-110 ${
+          props.className || 'px-2 py-1.5'
+        }`}
+      >
+        <span>Search artist...</span>
+        <MagnifyingGlassIcon className='ml-auto size-6 flex-none' />
+      </button>
+    </div>
+  )
+}
